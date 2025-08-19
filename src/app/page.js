267ba816b2e5aac1344dba19887681007cbc8e50
haimomo24 <135099315+haimomo24@users.noreply.github.com>
2025-08-19 13:55:15 +0700

    push

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHome from './component/homepage/PageHome'
+
+const page = () => {
+  return (
+    <div>
+      <PageHome/>
+    </div>
+  )
+}
+
+export default page
